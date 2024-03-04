f3faeaa79ee295309eb4f9fdec699e500e1e6b54
Michael Barbuzano <148410804+MichaelBarbuzano@users.noreply.github.com>
2024-03-04 02:21:36 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  // Add this configuration
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      // Add other routes here if needed
+    };
+  },
+  // Add this configuration to enable static HTML export
+  target: 'serverless',
+  // Add this configuration for Next.js 11 and later versions
+  // If you are using an older version of Next.js, you may not need this line
+  experimental: { output: 'export' },
+};
